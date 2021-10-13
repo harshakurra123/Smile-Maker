a6624e7b57abb959db6851ab54323ff49682cbfc
Kurra <S542409@nwmissouri.edu>
2021-10-13 09:16:56 -0500

    changes in api

--- smilemaker/smilemaker/wsgi.py
+++ smilemaker/smilemaker/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "smilemaker.smilemaker.settings" 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smilemaker.smilemaker.settings')
+# os.environ["DJANGO_SETTINGS_MODULE"] = "smilemaker.settings" 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smilemaker.settings')
 
 application = get_wsgi_application()
